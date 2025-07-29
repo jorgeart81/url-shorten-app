37876afd80e76d2cd188ec9ffbfb967c136182d6
Jorge Reyes <jorgeart81dev@gmail.com>
2025-07-28 19:43:52 -0500

    feat: Add AuthLayout

---
+++ src/modules/auth/AuthLayout.tsx
@@ -0,0 +1,31 @@
+import { GalleryVerticalEnd } from 'lucide-react';
+import { Navigate, Outlet } from 'react-router';
+import { useAuthStore } from './store/authStore';
+import { RoutePath } from '@/shared/constants/routePath';
+
+export const AuthLayout = () => {
+  const status = useAuthStore((state) => state.status);
+  const resendCode = useAuthStore((state) => state.resendCode);
+
+  if (status === 'authenticated') {
+    return <Navigate to={RoutePath.Home} />;
+  }
+  
+  if (status === 'unconfirmedEmail' && resendCode != null) {
+    return <Navigate to={RoutePath.ResendConfirmation} />;
+  }
+
+  return (
+    <div className='flex min-h-dvh flex-col items-center justify-center gap-6 p-6 md:p-10'>
+      <div className='flex w-full max-w-sm flex-col gap-6'>
+        <a href='#' className='flex items-center gap-2 self-center font-medium'>
+          <div className='bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md'>
+            <GalleryVerticalEnd className='size-4' />
+          </div>
+          Url Shorten{}
+        </a>
+        <Outlet />
+      </div>
+    </div>
+  );
+};
