30a55d7546bce5f6fbc6767c4e85aecca0121931
Jorge Reyes <jorgeart81dev@gmail.com>
2025-08-05 16:05:00 -0500

    feat: Add ViewHeader

---
+++ src/modules/dashboard/components/ViewHeader.tsx
@@ -0,0 +1,24 @@
+import React, { type JSX } from 'react';
+import { twMerge } from 'tailwind-merge';
+
+interface Props extends React.ComponentProps<'header'> {
+  title: string;
+  button?: JSX.Element;
+}
+
+export const ViewHeader = ({ button, title, className, ...props }: Props) => {
+  return (
+    <header
+      className={twMerge(
+        'flex flex-wrap items-center gap-2 justify-between',
+        className
+      )}
+      {...props}
+    >
+      <h1 className='scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0'>
+        {title}
+      </h1>
+      {button}
+    </header>
+  );
+};
