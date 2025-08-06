8ce3b15daadadaf19eeb9336ac9054525fca0a9e
Jorge Reyes <92123185+jorgeart81@users.noreply.github.com>
2025-08-05 20:05:26 -0500

    style: Add ViewContainer padding

--- src/modules/dashboard/components/ViewContainer.tsx
+++ src/modules/dashboard/components/ViewContainer.tsx
@@ -5,7 +5,7 @@ export const ViewContainer: FC<React.ComponentProps<'div'>> = ({
 }) => {
   return (
     <div className='flex items-center flex-1 flex-col gap-4 p-4 pt-0'>
-      <div className='container min-h-full'>{children}</div>
+      <div className='container min-h-full md:px-10 lg:px-16'>{children}</div>
     </div>
   );
 };
