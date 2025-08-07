9eec9b97522154966eaafb324058fd3c32b68ffc
Jorge Reyes <jorgeart81dev@gmail.com>
2025-08-07 13:22:17 -0500

    refactor: Set ErrorBoundary in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,36 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router';
 
-import { AuthModule } from './modules/auth/AuthModule';
 import { NotFoundView } from './components/NotFoundView';
-import { HomeView } from './modules/dashboard/views/HomeView';
+import { AuthModule } from './modules/auth/AuthModule';
 import { DashboardLayout } from './modules/dashboard/components/DashboardLayout';
-import { LinkView } from './modules/dashboard/views/LinkView';
 import { CreateLinkView } from './modules/dashboard/views/CreateLinkView';
+import { HomeView } from './modules/dashboard/views/HomeView';
 import { LinkDetailsView } from './modules/dashboard/views/LinkDetailsView';
+import { LinkView } from './modules/dashboard/views/LinkView';
+import { ErrorBoundary } from './components/ErrorBoundary';
+import { FallbackView } from './components/FallbackView';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Navigate to='/auth' />} />
-        <Route path='/auth/*' element={<AuthModule />} />
-        <Route path='*' element={<NotFoundView />} />
+    <ErrorBoundary fallback={<FallbackView />}>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate to='/auth' />} />
+          <Route path='/auth/*' element={<AuthModule />} />
+          <Route path='*' element={<NotFoundView />} />
 
-        <Route element={<DashboardLayout />}>
-          <Route path='/home' element={<HomeView />} />
-          <Route path='/links' element={<LinkView />} />
-          <Route path='/links/create' element={<CreateLinkView />} />
-          <Route path='/links/:backHalf/details' element={<LinkDetailsView />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+          <Route element={<DashboardLayout />}>
+            <Route path='/home' element={<HomeView />} />
+            <Route path='/links' element={<LinkView />} />
+            <Route path='/links/create' element={<CreateLinkView />} />
+            <Route
+              path='/links/:backHalf/details'
+              element={<LinkDetailsView />}
+            />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ErrorBoundary>
   );
 }
 
--- src/modules/auth/AuthModule.tsx
+++ src/modules/auth/AuthModule.tsx
@@ -1,7 +1,5 @@
 import { Navigate, Route, Routes } from 'react-router';
 
-import { ErrorBoundary } from '@/components/ErrorBoundary.tsx';
-import { FallbackView } from '@/components/FallbackView.tsx';
 import { NotFoundView } from '@/components/NotFoundView.tsx';
 import { AuthLayout } from './components/AuthLayout';
 import { EmailConfirmationView } from './views/EmailConfirmationView.tsx';
@@ -11,18 +9,16 @@ import { SignupView } from './views/SignupView.tsx';
 
 export const AuthModule = () => {
   return (
-    <ErrorBoundary fallback={<FallbackView />}>
-      <Routes>
-        <Route element={<AuthLayout />}>
-          <Route index element={<Navigate to='login' replace />} />
-          <Route path='login' element={<LoginView />} />
-          <Route path='signup' element={<SignupView />} />
-        </Route>
+    <Routes>
+      <Route element={<AuthLayout />}>
+        <Route index element={<Navigate to='login' replace />} />
+        <Route path='login' element={<LoginView />} />
+        <Route path='signup' element={<SignupView />} />
+      </Route>
 
-        <Route path='confirm-Email' element={<EmailConfirmationView />} />
-        <Route path='resend-confirmation' element={<ResendView />} />
-        <Route path='*' element={<NotFoundView />} />
-      </Routes>
-    </ErrorBoundary>
+      <Route path='confirm-Email' element={<EmailConfirmationView />} />
+      <Route path='resend-confirmation' element={<ResendView />} />
+      <Route path='*' element={<NotFoundView />} />
+    </Routes>
   );
 };
