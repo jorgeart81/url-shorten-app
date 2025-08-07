2f35b623f2765f82ac3bd919d7fe2b09a1d12f78
Jorge Reyes <jorgeart81dev@gmail.com>
2025-08-07 15:51:42 -0500

    improve LinkAction.tsx

--- src/modules/dashboard/components/linkCard/LinkCardAction.tsx
+++ src/modules/dashboard/components/linkCard/LinkCardAction.tsx
@@ -2,12 +2,30 @@ import type { FC, MouseEventHandler } from 'react';
 
 import { Button } from '@/components/ui/button';
 import { CardAction } from '@/components/ui/card';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import {
+  Copy,
+  EllipsisVertical,
+  Link2,
+  Pencil,
+  Share2,
+  Trash,
+} from 'lucide-react';
+import { useLanguage } from '@/components/hooks/useLanguage';
 
 interface Props {
   className?: string;
   handleCopy: MouseEventHandler;
   handleEdit: MouseEventHandler;
   handleShare: MouseEventHandler;
+  handleDelete: MouseEventHandler;
+  handleDetails?: MouseEventHandler;
 }
 
 export const LinkCardAction: FC<Props> = ({
@@ -15,18 +33,54 @@ export const LinkCardAction: FC<Props> = ({
   handleCopy,
   handleEdit,
   handleShare,
+  handleDelete,
+  handleDetails,
 }) => {
+  const { translate: t } = useLanguage();
   return (
     <CardAction className={className}>
-      <Button className='flex-1 md:flex-none' variant='secondary' onClick={handleCopy}>
-        Copy
+      <Button
+        className='flex-1 md:flex-none'
+        variant='secondary'
+        onClick={handleCopy}
+      >
+        <Copy /> {t('copy')}
       </Button>
       <Button variant='outline' onClick={handleShare}>
-        Share
+        <Share2 /> {t('share')}
       </Button>
-      <Button variant='outline' onClick={handleEdit}>
-        Edit
+      <Button
+        title={t('edit').toLocaleLowerCase()}
+        aria-label={`${t('edit')} link`}
+        variant='outline'
+        onClick={handleEdit}
+      >
+        <Pencil />
       </Button>
+
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            title={t('moreOptions').toLocaleLowerCase()}
+            aria-label={t('more')}
+            variant='outline'
+          >
+            <EllipsisVertical />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className='w-56' align='start'>
+          <DropdownMenuGroup>
+            <DropdownMenuItem onClick={handleDelete}>
+              <Trash /> {t('delete')}
+            </DropdownMenuItem>
+            {handleDetails && (
+              <DropdownMenuItem onClick={handleDetails}>
+                <Link2 className='rotate-45' /> {t('view.link.details')}
+              </DropdownMenuItem>
+            )}
+          </DropdownMenuGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </CardAction>
   );
 };
