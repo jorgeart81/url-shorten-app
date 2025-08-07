70587a5b009fb27aa49f0084db658be6f30bbd37
Jorge Reyes <jorgeart81dev@gmail.com>
2025-08-07 13:20:41 -0500

    style: FallbackView

--- src/components/FallbackView.tsx
+++ src/components/FallbackView.tsx
@@ -6,21 +6,23 @@ export const FallbackView = () => {
   const { translate } = useLanguage();
 
   return (
-    <section className='size-full p-6 content-center'>
-      <div className='space-y-1'>
-        <h1 className='scroll-m-20 text-2xl font-semibold tracking-tight text-center'>
-          {translate('errorBoundary.title')}
-        </h1>
+    <section className='h-dvh p-6 flex justify-center items-center'>
+      <div className='max-w-lg'>
+        <div className='space-y-1'>
+          <h1 className='scroll-m-20 text-2xl font-semibold tracking-tight text-center'>
+            {translate('errorBoundary.title')}
+          </h1>
 
-        <p className='text-muted-foreground text-sm m-2 text-center'>
-          {translate('errorBoundary.description')}
-        </p>
-      </div>
-      <Separator className='my-4' />
-      <div className='flex h-5 items-center justify-center space-x-4 text-sm'>
-        <Button variant='ghost' onClick={() => window.location.reload()}>
-          {translate('errorBoundary.reloadButton')}
-        </Button>
+          <p className='text-muted-foreground text-sm m-2 text-center'>
+            {translate('errorBoundary.description')}
+          </p>
+        </div>
+        <Separator className='my-4' />
+        <div className='flex h-5 items-center justify-center space-x-4 text-sm'>
+          <Button variant='ghost' onClick={() => window.location.reload()}>
+            {translate('errorBoundary.reloadButton')}
+          </Button>
+        </div>
       </div>
     </section>
   );
