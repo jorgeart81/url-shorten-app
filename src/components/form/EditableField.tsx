8838a7157f1953ac856ef9e4e3f540f5f5f62137
Jorge Reyes <jorgeart81dev@gmail.com>
2025-08-18 22:44:49 -0500

    feat: Add EditableField

---
+++ src/components/form/EditableField.tsx
@@ -0,0 +1,63 @@
+import {
+  useState,
+  type FC,
+  type ReactNode
+} from 'react';
+
+import { Button } from '@/components/ui/button';
+import { CustomInput } from './CustomInput';
+
+interface Props {
+  name: string;
+  label: string;
+  value: string;
+  buttonTitle: string;
+  buttonAriaLabel: string;
+  icon: ReactNode;
+  buttonText: string;
+}
+
+export const EditableField: FC<Props> = ({
+  name,
+  label,
+  value,
+  buttonTitle,
+  buttonAriaLabel,
+  icon,
+  buttonText,
+}) => {
+  const [canEdit, setCanEdit] = useState(false);
+  const [inputValue, setInputValue] = useState(value);
+
+  return (
+    <div>
+      <label className='scroll-m-20 font-semibold tracking-tight'>
+        {label}
+      </label>
+      <div className='flex items-center gap-x-4'>
+        {canEdit ? (
+          <CustomInput
+            id={name}
+            name={name}
+            type='text'
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+        ) : (
+          <>
+            <p>{value}</p>
+            <Button
+              title={buttonTitle}
+              aria-label={buttonAriaLabel}
+              variant='ghost'
+              onClick={() => setCanEdit(true)}
+            >
+              {icon}
+              {buttonText}
+            </Button>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
