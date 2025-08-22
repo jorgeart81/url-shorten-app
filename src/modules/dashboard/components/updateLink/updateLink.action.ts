a5fa7fd6eb11e497b2a826da503ef7e5096f7177
Jorge Reyes <92123185+jorgeart81@users.noreply.github.com>
2025-08-22 00:43:13 -0500

    feat: Add updateLinkAction

---
+++ src/modules/dashboard/components/updateLink/updateLink.action.ts
@@ -0,0 +1,66 @@
+import z from 'zod/v4';
+
+import { LinkService } from '../../services/links/linkService';
+import {
+  updateLinkSchema,
+  type UpdateLinkValidationError,
+} from './updateLinkValidation';
+
+import type { JsonPatchOperation } from '@/config/types/jsonPatchDocument';
+import type { Link } from '../../store/types/link';
+import type { UpdateState, UpdateStatus } from './LinkEditForm';
+
+const formDataValidation = (data: { [k: string]: FormDataEntryValue }) => {
+  const validationResult = updateLinkSchema.safeParse(data);
+
+  if (!validationResult.success || validationResult.error) {
+    const errors = z.treeifyError(validationResult.error)
+      .properties as UpdateLinkValidationError;
+
+    return { hasErrors: true, errors: errors };
+  }
+  return { hasErrors: false, errors: undefined };
+};
+
+const changeData = (data: { [k: string]: FormDataEntryValue }, link: Link) => {
+  const changeEntries = Object.entries(data).filter(
+    ([key, value]) => key in link && value !== link[key as keyof Link]
+  );
+
+  return { hasChanges: changeEntries.length > 0, changeEntries };
+};
+
+export const updateLinkAction = async (
+  state: UpdateState,
+  queryData: FormData
+) => {
+  let status: UpdateStatus = state.status;
+  const formData = Object.fromEntries(queryData);
+
+  const { hasChanges, changeEntries } = changeData(formData, state.link);
+  if (!hasChanges) {
+    status = 'noChanges';
+    return { ...state, status };
+  }
+
+  const { hasErrors, errors } = formDataValidation(formData);
+  if (hasErrors) return { ...state, validationError: errors };
+
+  const patchOperations: JsonPatchOperation[] = changeEntries.map(
+    ([key, value]) => ({
+      op: 'replace',
+      path: `/${key}`,
+      value,
+    })
+  );
+
+  const result = await LinkService.partialUpdate(
+    state.link.id,
+    patchOperations
+  );
+  status = result.success ? 'success' : 'fail';
+  return {
+    ...state,
+    status,
+  };
+};
