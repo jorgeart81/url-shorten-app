5dbb0fa5cdd90b1b139f8ac22cf2ba7fddac0950
Jorge Reyes <jorgeart81dev@gmail.com>
2025-08-26 17:53:09 -0500

    feat: Add createLinkAction

---
+++ src/modules/dashboard/components/createLink/createLink.action.ts
@@ -0,0 +1,49 @@
+import z from 'zod/v4';
+
+import { RoutePath } from '@/shared/constants/routePath';
+import { LinkService } from '../../services/links/linkService';
+import {
+  createLinkSchema,
+  type CreateLinkValidationError,
+} from './createLinkValidation';
+import type { CreateState } from './QuickCreateLink';
+
+const formDataValidation = (data: { [k: string]: FormDataEntryValue }) => {
+  const validationResult = createLinkSchema.safeParse(data);
+
+  if (!validationResult.success || validationResult.error) {
+    const errors = z.treeifyError(validationResult.error)
+      .properties as CreateLinkValidationError;
+
+    return { hasErrors: true, errors: errors, data: validationResult.data };
+  }
+  return { hasErrors: false, errors: undefined, data: validationResult.data };
+};
+
+export const createLinkAction = async (
+  state: CreateState,
+  queryData: FormData
+) => {
+  const formData = Object.fromEntries(queryData);
+  const { hasErrors, errors, data } = formDataValidation(formData);
+  
+  console.log(formData)
+  if (hasErrors || !data) return { ...state, validationError: errors };
+  
+  const { backHalf, destination, ...rest } = data;
+  
+  const { success, value } = await LinkService.createLink({
+    ...rest,
+    backHalf: backHalf && backHalf.trim().length > 0 ? backHalf : undefined,
+    destination: destination,
+  });
+
+  if (success && value?.data.backHalf) {
+    return {
+      ...state,
+      goTo: `${RoutePath.Links}/${value.data.backHalf}/details`,
+    };
+  }
+
+  return state
+};
