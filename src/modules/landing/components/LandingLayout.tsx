4c4562c0b0a03a32fd3466186f7f953be115c976
Jorge Reyes <jorgeart81dev@gmail.com>
2025-09-04 23:24:29 -0500

    style: Add layout.module.css

--- src/modules/landing/components/LandingLayout.tsx
+++ src/modules/landing/components/LandingLayout.tsx
@@ -4,10 +4,14 @@ import { Button } from '@/components/ui/button';
 import { RoutePath } from '@/shared/constants/routePath';
 import { useLanguage } from '@/components/hooks/useLanguage';
 
+import styles from './layout.module.css';
+
 export const LandingLayout = () => {
   const { translate: t } = useLanguage();
   return (
-    <div className='w-screen min-h-dvh flex flex-col items-center'>
+    <div
+      className={`w-screen min-h-dvh flex flex-col items-center ${styles['bg-container']}`}
+    >
       <nav className='w-full max-w-[1440px] h-16 flex gap-3 py-4 px-6'>
         <div className='flex-1'>
           <h3 className='scroll-m-20 text-xl font-semibold tracking-tight'>
@@ -30,7 +34,7 @@ export const LandingLayout = () => {
       <div className='relative w-full max-w-6xl flex-1'>
         <Outlet />
       </div>
-      <footer className='mt-10 text-sm text-muted-foreground'>
+      <footer className='flex items-center h-12 text-sm text-muted-foreground'>
         &copy; 2025 URL Shorten App. All rights reserved.
       </footer>
     </div>
--- src/modules/landing/views/LandingView.tsx
+++ src/modules/landing/views/LandingView.tsx
@@ -1,29 +1,25 @@
 import type { FC } from 'react';
 
-import styles from './Landing.module.css';
-
 export const LandingView: FC = () => (
   <main>
-    <header className={`${styles.header}`}>
+    <header className='relative flex flex-col justify-center text-center border-1 min-h-[calc(100vh-112px)]'>
       <h1
-        className='relative text-3xl font-bold mb-4 mt-16 z-10'
+        className='relative text-3xl font-bold mb-4 mt-16'
         style={{ fontSize: 'max(24px, min(3.75vw, 48px))' }}
       >
         Welcome to URL Shorten App
       </h1>
 
-      <div className='relative flex flex-col items-center text-muted-foreground z-10'>
+      <div className='relative flex flex-col items-center'>
         <p className='mb-4 text-xl'>
           Create, manage, and share your short links easily.
         </p>
-        <ul className='list-disc pl-6 w-fit text-left'>
+        <ul className='list-disc pl-6 w-fit text-left leading-7'>
           <li>Fast and secure link shortening</li>
           <li>Custom back-half support</li>
           <li>Link analytics and management</li>
         </ul>
       </div>
     </header>
-
-    <section></section>
   </main>
 );
